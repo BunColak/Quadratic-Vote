90b92c21947c88887309d7deaf3cef6b2d4cd8bd
Bun Colak <bun.colak@motius.de>
2023-01-02 15:02:54 +0100

    e2e test init

---
+++ e2e/create.spec.ts
@@ -0,0 +1,9 @@
+import test from "@playwright/test";
+
+test.describe('create poll', () => {
+    test('should allow adding new options', async ({browser}) => {})
+    test('should allow removing options', async ({browser}) => {})
+    test('should validate options', async ({browser}) => {})
+    test('should submit form and redirect if successfull', async ({browser}) => {})
+    test('should submit form and throw error if any', async ({browser}) => {})
+})
---
+++ e2e/overview.spec.ts
@@ -0,0 +1,11 @@
+import test from "@playwright/test";
+
+test.describe('poll overview', () => {
+    test('should show graph if there are any votes', async ({browser}) => {})
+    test('should show no votes text if there is no votes yet', async ({browser}) => {})
+    test('should show invite link', async ({browser}) => {})
+    test('should copy invite link to clipboard on click', async ({browser}) => {})
+    test('should validate if user is part of the poll', async ({browser}) => {})
+    test('should show my poll page', async ({browser}) => {})
+    test('should show edit button if user is the author', async ({browser}) => {})
+})
